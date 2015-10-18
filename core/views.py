7a54df12f0c4502f3a4c548d1535011f50d1f29f
sbjohnson <sbjohnson@quinnipiac.edu>
2015-10-18 21:39:14 +0000

    Add answer create view

--- core/views.py
+++ core/views.py
@@ -25,6 +25,13 @@ class QuestionDetailView(DetailView):
     model = Question
     template_name = 'question/question_detail.html'
 
+    def get_context_data(self, **kwargs):
+        context = super(QuestionDetailView, self).get_context_data(**kwargs)
+        question = Question.objects.get(id=self.kwargs['pk'])
+        answers = Answer.objects.filter(question=question)
+        context['answers'] = answers
+        return context
+    
 class QuestionUpdateView(UpdateView):
     model = Question
     template_name = 'question/question_form.html'
@@ -34,4 +41,17 @@ class QuestionDeleteView(DeleteView):
     model = Question
     template_name = 'question/question_confirm_delete.html'
     success_url = reverse_lazy('question_list')
+
+class AnswerCreateView(CreateView):
+    model = Answer
+    template_name = 'answer/answer_form.html'
+    fields = ['text']
+
+    def get_success_url(self):
+        return self.object.question.get_absolute_url()
+    
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        form.instance.question = Question.objects.get(id=self.kwargs['pk'])
+        return super(AnswerCreateView, self).form_valid(form)
     
